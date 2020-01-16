4137b2ac29bf8cd22ed6720c8751177a9409eeb9
Korina Pluss <korina.pluss@gmail.com>
2020-01-16 09:52:36 -0500

    remove commented out code

--- src/app.js
+++ src/app.js
@@ -20,23 +20,7 @@ app.get('/', (req, res) => {
     })
 })
 
-
 app.use('/account', accountRoutes)
-// app.get('/savings', (req, res) => {
-//     res.render('account', {
-//         account : accounts.savings
-//     })
-// })
-// app.get('/checking', (req, res) => {
-//     res.render('account', {
-//         account : accounts.checking
-//     })
-// })
-// app.get('/credit', (req, res) => {
-//     res.render('account', {
-//         account : accounts.credit
-//     })
-// })
 
 app.get('/profile', (req, res) => {
     res.render('profile', {
@@ -45,34 +29,6 @@ app.get('/profile', (req, res) => {
 })
 
 app.use('/services', servicesRoutes)
-// app.get('/payment', (req, res) => {
-//     res.render('payment', {
-//         account: accounts.credit
-//     })
-// })
-
-// app.post('/payment', (req, res) => {
-//     accounts.credit.balance -= parseInt(req.body.amount)
-//     accounts.credit.available += parseInt(req.body.amount)
-//     writeJSON()
-//     res.render('payment', {
-//         message : "Payment successful",
-//         account: accounts.credit
-//     })
-// })
-
-// app.get('/transfer', (req, res) => {
-//     res.render('transfer')
-// })
-
-// app.post('/transfer', (req, res) => {
-//     accounts[req.body.from].balance -= parseInt(req.body.amount)
-//     accounts[req.body.to].balance += parseInt(req.body.amount)
-//     writeJSON()
-//     res.render('transfer', {
-//         message : "Transfer Completed"
-//     })
-// })
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!')
